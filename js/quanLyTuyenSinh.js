de10c5a1d197d9e3c37f396e38089bb87de7786a
demen84 <quyit84@gmail.com>
2025-05-01 22:31:00 +0700

    abc

---
+++ js/quanLyTuyenSinh.js
@@ -0,0 +1,37 @@
+/*
+* Đầu vào:
+    - Điểm chuẩn
+    - Điểm môn 1, môn 2, môn 3
+    - Khu vực ưu tiên: nhập X nếu không thuộc khu vực ưu tiên. Khu vực A +2đ, B +1đ, C +0.5đ.
+    - Đối tượng ưu tiên: nhập 0 nếu không thuộc đối tượng ưu tiên. Đối tượng 1 +2.5đ, 2 +1.5đ, 3 +1đ.
+* Xử lý:
+    - Nếu điểm tổng 3 môn >= điểm chuẩn & không có môn nào bị 0đ thì thí sinh đậu. Else rớt.
+    - Tính tổng điểm của thí sinh = (Điểm môn 1 + Điểm môn 2 + Điểm môn 3) + (Điểm ưu tiên khu vực + Điểm ưu tiên đối tượng).
+* Đầu ra:
+    - Tổng số điểm của thí sinh.
+    - Kết quả Đậu/Rớt của thí sinh.
+*/
+
+//Hàm tính tổng điểm của thí sinh
+function tinhTongDiem(diemMon1, diemMon2, diemMon3, khuVuc, doiTuong)
+{
+    let tongDiem = diemMon1 + diemMon2 + diemMon3;
+    if (khuVuc === 'A') {
+        tongDiem += 2;
+    } else if (khuVuc === 'B') {
+        tongDiem += 1;
+    } else if (khuVuc === 'C') {
+        tongDiem += 0.5;
+    }
+
+    if (doiTuong === 1) {
+        tongDiem += 2.5;
+    } else if (doiTuong === 2) {
+        tongDiem += 1.5;
+    } else if (doiTuong === 3) {
+        tongDiem += 1;
+    }
+
+    return tongDiem;
+}
+
